179c82f0040f7b7628b3aaee5618e8cd4e474e35
Ivy <ivyjkemboi@gmail.com>
2024-09-11 13:13:22 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -1,15 +1,11 @@
-#!/usr/bin/env python3
-
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Plant
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -18,36 +14,46 @@ api = Api(app)
 
 
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
 
     def post(self):
         data = request.get_json()
-
         new_plant = Plant(
             name=data['name'],
             image=data['image'],
             price=data['price'],
         )
-
         db.session.add(new_plant)
         db.session.commit()
-
         return make_response(new_plant.to_dict(), 201)
 
 
-api.add_resource(Plants, '/plants')
-
-
 class PlantByID(Resource):
-
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
+        plant = Plant.query.get_or_404(id).to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.get_or_404(id)
+        data = request.get_json()
+        plant.price = data.get('price', plant.price)
+        plant.is_in_stock = data.get('is_in_stock', plant.is_in_stock)
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
 
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+            return '', 204  # Return an empty response with 204 No Content
+        return make_response(jsonify({"message": "Plant not found"}), 404)
+
+
+
+api.add_resource(Plants, '/plants')
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
